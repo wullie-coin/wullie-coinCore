f8537ae506c4ab2722e4c17c271719dacbfb8707
wullie-coin <83089373+wullie-coin@users.noreply.github.com>
2021-04-26 20:24:32 +0000

    Add files via upload

---
+++ nnp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	printf("thankyou for being a part of wullie coin\n");
+	printf("you will be payed if you are mining and running a node\n");
+	
+	
+	
+	return 0;
+}
+
+
+int gettingUserId(){
+
+
+
+
+
+
+
+
+}
+
